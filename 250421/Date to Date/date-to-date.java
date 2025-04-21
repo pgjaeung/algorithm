c94c7fb61cf9622f4ca2798bfd4d65651ff8d7ec
pgjaeung <64916999+pgjaeung@users.noreply.github.com>
2025-04-21 18:45:46 +0900

    Title: Date to Date, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250421/Date to Date/date-to-date.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        // Please write your code here.
+        int date[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        int sum=0;
+        if(m1<=m2){
+            for(int i=m1; i<=m2; i++){
+                if(i==m2) sum+=d2;
+                else if(i==m1) sum+=date[m1-1]-d1;
+                else sum+=date[i-1];
+            } 
+        }
+        System.out.println(sum+1);
+    }
+}
